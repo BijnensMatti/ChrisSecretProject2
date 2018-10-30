b737f96141d63bdd4daeb41962259db7c9825bf3
christiankrug <mrminifreddy@gmail.com>
2018-10-30 16:32:15 +0100

    Notification Sender

---
+++ notify.php
@@ -0,0 +1,15 @@
+<?php
+$payload = array(
+    'apikey' => "xxxxx"
+    'url' => 'https://example.com/breaking-news/',
+    'content' => 'Check this out! #flightdeal'
+);
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "https://pushapp.pro/publisherapi/pushes/");
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($payload));
+$result = curl_exec($ch);
+?>
